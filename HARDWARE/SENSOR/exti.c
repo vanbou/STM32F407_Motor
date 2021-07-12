1a42e4962ce84718f814fd6f75c33bd0c1d3dc2f
wanbo <44627351+vanbou@users.noreply.github.com>
2021-07-12 10:19:39 +0800

    Update exti.c

--- HARDWARE/SENSOR/exti.c
+++ HARDWARE/SENSOR/exti.c
@@ -10,102 +10,13 @@
 
 
 
-//传感器状态监测,逆时针
-u8 Check_Sensor0_State(void)
-{
-	 
-    //Sensor1触发情况
-    if(GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_0) == SET) 
-    {
-			printf("\n----------Sensor 1 No Trigger---------\n");
-      return NORMAL;
-    }
-    //触发
-    else
-    {
-			printf("\n----------Sensor 1 has Trigger---------\n");
-      return TAGGLE;    
-    }  
-		//delay_ms(10);
-}
-
-
-	//顺时针
-u8 Check_Sensor1_State(void)
-{
-		//Sensor2触发情况
-    if(GPIO_ReadInputDataBit(GPIOG, GPIO_Pin_1) == SET) 
-    {
-			printf("\n----------Sensor 2 No Trigger---------\n");
-      return NORMAL;
-    }
-    //触发
-    else
-    {
-			printf("\n----------Sensor 2 has Trigger---------\n");
-      return TAGGLE;    
-    }  
-		//delay_ms(10);
-	}
-
-	
-	//下方
-	u8 Check_Sensor2_State(void)
-{
-		
-				//Sensor3触发情况
-    if(GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_2) == SET) 
-    {
-			printf("\n----------Sensor 3 No Trigger---------\n");
-      return NORMAL;
-    }
-    //触发
-    else
-    {
-			printf("\n----------Sensor 3 has Trigger---------\n");
-      return TAGGLE;    
-    } 
-		//delay_ms(10);
-		
-	}
-
-	
-//上方
-	u8 Check_Sensor3_State(void)
-{
-		
-				//Sensor4触发情况
-    if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_3) == SET) 
-    {
-			printf("\n----------4No---------\n");
-      return NORMAL;
-    }
-    //触发
-    else
-    {
-			printf("\n----------Sensor 4 has Trigger---------\n");
-      return TAGGLE;    
-    } 
-		
-		//delay_ms(10);
- }
-
-
-
-
-
-
-
-
-
-
 
 //外部中断0服务程序 右边传感器中断
 void EXTI0_IRQHandler(void)
 {
   if(EXTI_GetITStatus(EXTI_Line0) == 1)
   {
-		printf("--------exit0-------------");
+		//printf("--------exit0-------------");
 		
     EXTI_ClearITPendingBit(EXTI_Line0);
   }//清除LINE0上的中断标志位 
@@ -113,13 +24,13 @@ void EXTI0_IRQHandler(void)
 
 
 //外部中断1服务程序 左边传感器中断
-void EXTI1_IRQHandler(void)
+void EXTI15_10_IRQHandler(void)
 {
-	 if(EXTI_GetITStatus(EXTI_Line1) == 1)
+	 if(EXTI_GetITStatus(EXTI_Line11) == 1)
 	  {
-		printf("--------exit1-------------");
+		//printf("--------exit1-------------");
 		
-    EXTI_ClearITPendingBit(EXTI_Line1);
+    EXTI_ClearITPendingBit(EXTI_Line11);
   }//清除LINE1上的中断标志位 
 }
 
@@ -130,7 +41,7 @@ void EXTI2_IRQHandler(void)
 
 	 if(EXTI_GetITStatus(EXTI_Line2) == 1)
 	  {
-			printf("--------exit2-------------");
+			//printf("--------exit2-------------");
 		
     EXTI_ClearITPendingBit(EXTI_Line2);
   } //清除LINE2上的中断标志位  
@@ -143,7 +54,7 @@ void EXTI3_IRQHandler(void)
 
  if(EXTI_GetITStatus(EXTI_Line3) == 1)
 	 {
-		 printf("--------exit3-------------");
+		 //printf("--------exit3-------------");
 		
     EXTI_ClearITPendingBit(EXTI_Line3);
   }//清除LINE3上的中断标志位  
@@ -160,7 +71,7 @@ void EXTIX_Init(void)
 	GPIO_InitTypeDef   GPIO_InitStructure;
 	
 
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB|RCC_AHB1Periph_GPIOG|RCC_AHB1Periph_GPIOC|RCC_AHB1Periph_GPIOA, ENABLE);//使能GPIOB,GPIOG,GPIOC,GPIOA时钟
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB|RCC_AHB1Periph_GPIOF|RCC_AHB1Periph_GPIOC|RCC_AHB1Periph_GPIOA, ENABLE);//使能GPIOB,GPIOG,GPIOC,GPIOA时钟
  
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);//使能SYSCFG时钟（中断需要使用AFIO时钟)
 	
@@ -172,11 +83,11 @@ void EXTIX_Init(void)
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
   GPIO_Init(GPIOB, &GPIO_InitStructure);//初始化GPIOB0
 	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1; //PG1对应IO配置
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11; //PG1对应IO配置
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;//普通输入模式
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100M
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
-  GPIO_Init(GPIOG, &GPIO_InitStructure);//初始化GPIOG1
+  GPIO_Init(GPIOF, &GPIO_InitStructure);//初始化GPIOG1
 	
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2; //PC2对应IO配置
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;//普通输入模式
@@ -194,12 +105,12 @@ void EXTIX_Init(void)
  
  
 	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOB, EXTI_PinSource0);//PB0 连接到中断线0
-	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOG, EXTI_PinSource1);//PG1 连接到中断线1
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOF, EXTI_PinSource11);//PF11 连接到中断线1
 	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOC, EXTI_PinSource2);//PC2 连接到中断线2
 	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource3);//PA3 连接到中断线3
 	
 	//配置中断线
-  EXTI_InitStructure.EXTI_Line = EXTI_Line0|EXTI_Line1|EXTI_Line2|EXTI_Line3;//LINE0
+  EXTI_InitStructure.EXTI_Line = EXTI_Line0|EXTI_Line11|EXTI_Line2|EXTI_Line3;//LINE0
 	
   EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;//中断事件
 	//初始状态为低电平，当检测到物体时，KEY0输出高电平，此时INT0导通，变为低电平，即从上升沿跳变至下降沿时触发
@@ -215,7 +126,7 @@ void EXTIX_Init(void)
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;//使能外部中断通道
   NVIC_Init(&NVIC_InitStructure);//配置
 	
-	NVIC_InitStructure.NVIC_IRQChannel = EXTI1_IRQn;//外部中断2
+	NVIC_InitStructure.NVIC_IRQChannel = EXTI15_10_IRQn;//外部中断2
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00;//抢占优先级3
   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;//子优先级2
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;//使能外部中断通道
